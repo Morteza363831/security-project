67e1addcc0eea435db2707fef5ec561de22e3e97
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-17 14:46:09 +0430

    student controller and required prerequisites (html files) added

---
+++ src/main/java/com/example/securityproject/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.example.securityproject.controller;
+
+import com.example.securityproject.Dto.StudentDto;
+import com.example.securityproject.Repo.StudentRepo;
+import com.example.securityproject.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("login")
+    public String login(Model model) {
+        model.addAttribute("studentDto", new StudentDto());
+        return "login";
+    }
+
+    @GetMapping("home")
+    public String result(Model model) {
+        StudentDto studentDto= studentService.findStudentByUsername("mhzd");
+        model.addAttribute("studentDto", studentDto);
+        return "home";
+    }
+
+}
